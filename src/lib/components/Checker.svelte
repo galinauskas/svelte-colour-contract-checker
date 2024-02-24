5bc11a73f6c6dd055ba58cf5350fddab50388ac9
Rokas <galinauskas@protonmail.com>
2024-02-24 17:38:24 +0000

    Improved logic, formatted result

--- src/lib/components/Checker.svelte
+++ src/lib/components/Checker.svelte
@@ -1,4 +1,7 @@
 <script>
+  /*
+    Function to calculate the luminance of a colour
+  */
   function luminance(obj) {
     // iterate over colours in rgb object
     for (const value in obj) {
@@ -16,28 +19,50 @@
     return 0.2126 * obj.R + 0.7152 * obj.G + 0.0722 * obj.B;
   }
 
+  /*
+    Function to CALCULATE the contrast between two colours
+  */
   function contrast(x, y) {
-    // return contrast ratio
-    return Math.abs((x + 0.05) / (y + 0.05)).toFixed(2);
+    // divide into bigger figure
+    return x > y
+      ? Math.abs((x + 0.05) / (y + 0.05))
+      : Math.abs((y + 0.05) / (x + 0.05));
+  }
+
+  /*
+    Function to CHECK the contrast between two colours
+  */
+  function checkContrast(x, y) {
+    let l1 = luminance(x);
+    let l2 = luminance(y);
+
+    return contrast(l1, l2);
+  }
+
+  /*
+    Function to round a number to 2 decimal places
+  */
+  function round(x) {
+    return x % 1 !== 0 ? Number(x.toFixed(2)) : x;
   }
 
   let c1 = {
-    R: 62,
-    G: 62,
-    B: 62,
+    R: 0,
+    G: 0,
+    B: 0,
   };
 
   let c2 = {
-    R: 15,
-    G: 195,
-    B: 119,
+    R: 255,
+    G: 255,
+    B: 255,
   };
 
-  let l1 = luminance(c1);
-  let l2 = luminance(c2);
+  // calculate contrast ratio
+  let value = checkContrast(c1, c2);
 
-  // variable for display
-  let ratio = contrast(l2, l1);
+  // format value
+  value = round(value);
 </script>
 
-<h2>{ratio}:1</h2>
+<h2>{value}:1</h2>
