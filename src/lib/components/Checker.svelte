c7d4815209cb567d40a8c442313d72478c6df4c2
Rokas <galinauskas@protonmail.com>
2024-02-25 19:58:25 +0000

    Added user input function & cleaned up logic

--- src/lib/components/Checker.svelte
+++ src/lib/components/Checker.svelte
@@ -2,21 +2,21 @@
   /*
     Function to calculate the luminance of a colour
   */
-  function luminance(obj) {
+  function luminance(arr) {
     // iterate over colours in rgb object
-    for (const value in obj) {
+    for (let i = 0; i < 3; i++) {
       // convert to 0-1
-      let x = obj[value] / 255;
+      let x = arr[i] / 255;
 
       // apply gamma correction
       x > 0.03928 ? (x = Math.pow((x + 0.055) / 1.055, 2.4)) : (x /= 12.92);
 
       // change value in copy
-      obj[value] = x;
+      arr[i] = x;
     }
 
     // return luminance
-    return 0.2126 * obj.R + 0.7152 * obj.G + 0.0722 * obj.B;
+    return 0.2126 * arr[0] + 0.7152 * arr[1] + 0.0722 * arr[2];
   }
 
   /*
@@ -30,13 +30,52 @@
   }
 
   /*
+    Function to convert hex to rgb
+  */
+  function hexToRGB(str) {
+    const rgb = [];
+
+    // slice up string into pairs
+    for (let i = 0; i < str.length; i += 2) {
+      rgb.push(str.slice(i, i + 2));
+    }
+
+    // loop through each set of pairs
+    for (const element of rgb) {
+      // convert to number
+      let num = parseInt(element, 16);
+
+      // get index of set
+      let index = rgb.indexOf(element);
+
+      // change value in sets array
+      rgb[index] = num;
+    }
+
+    return rgb;
+  }
+
+  /*
     Function to CHECK the contrast between two colours
   */
-  function checkContrast(x, y) {
-    let l1 = luminance(x);
-    let l2 = luminance(y);
+  function calculate() {
+    // convert user input to rgb values
+    let rgb1 = hexToRGB(userInput1);
+    let rgb2 = hexToRGB(userInput2);
 
-    return contrast(l1, l2);
+    // calculate luminance of each value
+    let l1 = luminance(rgb1);
+    let l2 = luminance(rgb2);
+
+    // calculate contrast
+    let result = contrast(l1, l2);
+
+    // debuging
+    console.log(`The luminance of colour 1 is ${l1}`);
+    console.log(`The luminance of colour 2 is ${l2}`);
+    console.log(`The contrast between the two colours is ${round(result)}:1`);
+
+    return result;
   }
 
   /*
@@ -46,23 +85,19 @@
     return x % 1 !== 0 ? Number(x.toFixed(2)) : x;
   }
 
-  let c1 = {
-    R: 0,
-    G: 0,
-    B: 0,
-  };
-
-  let c2 = {
-    R: 255,
-    G: 255,
-    B: 255,
-  };
-
-  // calculate contrast ratio
-  let value = checkContrast(c1, c2);
-
-  // format value
-  value = round(value);
+  let userInput1 = "ffffff";
+  let userInput2 = "000000";
 </script>
 
-<h2>{value}:1</h2>
+<form>
+  <label for="colour1">Colour 1:</label>
+  <input type="text" id="colour1" bind:value={userInput1} />
+
+  <label for="colour2">Colour 2:</label>
+  <input type="text" id="colour2" bind:value={userInput2} />
+
+  <button on:click={calculate}>Calculate</button>
+</form>
+
+<h6>{userInput1}</h6>
+<h6>{userInput2}</h6>
